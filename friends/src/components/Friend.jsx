a611a7bb5e2c5619cc6bda6c70dbe005ef6b5b08
iykekings <iykekings36@gmail.com>
2019-06-13 16:09:34 +0100

    strecth complete

--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -5,12 +5,14 @@ const Friend = props => {
     return (
         <div className="friend">
             <p>{props.name}</p>
-            <p>{props.age}</p>
+            <p className="f-age">{props.age}</p>
             <p className="f-email">{props.email}</p>
-            <button onClick={(e) => props.deleteFriend(props.id) }>Delete</button>
-            <NavLink to={`/update-friend/${props.id}`} >
-                <button>Edit</button>
-            </NavLink>
+            <div className="f-btns">
+                <button onClick={(e) => props.deleteFriend(props.id) }>Delete</button>
+                <NavLink to={`/update-friend/${props.id}`} >
+                    <button>Edit</button>
+                </NavLink>
+            </div>
         </div>
     )
 }
--- friends/src/components/FriendList.jsx
+++ friends/src/components/FriendList.jsx
@@ -3,7 +3,7 @@ import Friend from './Friend'
 
 const FriendList = ({ friends, deleteFriend, editFriend }) => {
     return (
-        <div>
+        <div className="friend-con">
             {friends.map( friend => <Friend 
             key={friend.id} 
             {...friend} 
